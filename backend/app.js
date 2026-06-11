8c7154dfca2b0ed6fd22aa05d46dd6dd268b9300
fidel208 <dempafidel@gmail.com>
2026-06-11 11:27:18 +0300

    render

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
-const financeRoutes =  require("./routes/financeRoutes")
-require("dotenv").config();
+const financeRoutes = require("./routes/financeRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
